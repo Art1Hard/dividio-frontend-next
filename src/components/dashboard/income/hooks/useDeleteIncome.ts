41ce058b98b1d3945eeaae8034c578b90d511fcb
hardov <1artem1nazarov1@gmail.com>
2025-08-31 16:31:12 +0300

    fix: display error for last income

--- src/components/dashboard/income/hooks/useDeleteIncome.ts
+++ src/components/dashboard/income/hooks/useDeleteIncome.ts
@@ -1,5 +1,6 @@
 import { financeQueryKeys } from "@/constants/query-keys.constants";
 import incomeService from "@/services/income.service";
+import { isServerError } from "@/utils/server-error.utils";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { toast } from "sonner";
 
@@ -11,6 +12,14 @@ const useDeleteIncome = () => {
 		onSuccess: async (data) => {
 			await queryClient.invalidateQueries({ queryKey: financeQueryKeys.all });
 			toast.success(`Доход "${data.title}" успешно удален`);
+		},
+		onError: (e) => {
+			if (isServerError(e)) {
+				if (e.response!.data.code === "LAST_INCOME_VALIDATION_ERROR")
+					toast.error(
+						"Вы не можете удалить последний доход пока есть распределения!"
+					);
+			}
 		},
 	});
 
