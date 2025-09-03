0d1158f04ae6a520cb3550b18e341a07844c0188
hardov <1artem1nazarov1@gmail.com>
2025-09-03 17:16:21 +0300

    fix: fix not full color processing

--- src/utils/hexToRgb.ts
+++ src/utils/hexToRgb.ts
@@ -1,5 +1,13 @@
 export const hexToRgb = (hex: string) => {
 	hex = hex.replace("#", "");
+
+	if (hex.length === 3) {
+		hex = hex
+			.split("")
+			.map((char) => char + char)
+			.join("");
+	}
+
 	const r = parseInt(hex.substring(0, 2), 16);
 	const g = parseInt(hex.substring(2, 4), 16);
 	const b = parseInt(hex.substring(4, 6), 16);
