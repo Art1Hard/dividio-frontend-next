2a1df73fbeaaeae2ae50f47f4d5d0c2930188ef9
hardov <1artem1nazarov1@gmail.com>
2025-10-04 19:29:35 +0300

    fix: disable chart when no allocations

--- src/app/dashboard/allocation/AllocationWidget.tsx
+++ src/app/dashboard/allocation/AllocationWidget.tsx
@@ -7,16 +7,22 @@ import AllocationList from "@/components/dashboard/allocation/list/AllocationLis
 import CreateAllocation from "@/components/dashboard/allocation/create/CreateAllocation";
 import useChartView from "./hooks/useChartView";
 import AllocationChart from "@/components/dashboard/allocation/chart/AllocationChart";
+import useFetchAllocations from "@/hooks/useFetchAllocations";
 
 const AllocationWidget = () => {
 	const { isChartView, toggleChartView } = useChartView();
+	const { data } = useFetchAllocations();
 
 	return (
 		<DashboardWidgetWrapper
 			title="Распределение"
 			icon={<FaChartPie size={20} className="text-violet-500" />}
 			headerButton={
-				<ViewSwitcher isChartView={isChartView} onClick={toggleChartView} />
+				<ViewSwitcher
+					disabled={!data || data.length <= 1}
+					isChartView={isChartView}
+					onClick={toggleChartView}
+				/>
 			}>
 			<div className="space-y-5 mb-6">
 				{isChartView ? <AllocationChart /> : <AllocationList />}
